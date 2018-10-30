8fd179d84783de61cab1b9dda344184564764686
carlatovar@gmail.com <Buenos1981>
2018-10-30 11:52:51 -0400

    changes for class 1

--- HelloWold.py
+++ HelloWold.py
@@ -2,6 +2,6 @@ name = "Rafael"
 second = "garcia"
 number = 2
 
-result = (name.upper()+" "+second.upper()+"; and my favorite is "\
-         "number is" + str(number))
+result = (name.lower()+" "+second.upper()+"; and my favorite is " \
+         + "number is " + str(number))
 print(result)
